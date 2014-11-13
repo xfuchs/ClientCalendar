b5777e78ce3d6a5a92e9b9f7f5ef9a7ed17ff44f
xfuchs <helena-fuchs@hotmail.com>
2014-11-13 13:40:30 +0100

    App data

--- app.js
+++ app.js
@@ -1,51 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN""http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
-<html>
-<head>
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-  <title>Demo - jQuery Week Calendar</title>
-
-  <link rel='stylesheet' type='text/css' href='libs/css/smoothness/jquery-ui-1.8.11.custom.css' />
-  <link rel='stylesheet' type='text/css' href='jquery.weekcalendar.css' />
-  <style type='text/css'>
-  body {
-    font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
-    margin: 0;
-  }
-
-  h1 {
-    margin: 0 0 1em;
-    padding: 0.5em;
-  }
-
-  p.description {
-    font-size: 0.8em;
-    padding: 1em;
-    position: absolute;
-    top: 3.2em;
-    margin-right: 400px;
-  }
-
-  #message {
-    font-size: 0.7em;
-    position: absolute;
-    top: 1em;
-    right: 1em;
-    width: 350px;
-    display: none;
-    padding: 1em;
-    background: #ffc;
-    border: 1px solid #dda;
-  }
-  </style>
-
-  <script type='text/javascript' src='libs/jquery-1.4.4.min.js'></script>
-  <script type='text/javascript' src='libs/jquery-ui-1.8.11.custom.min.js'></script>
-
-  <script type="text/javascript" src="libs/date.js"></script>
-  <script type='text/javascript' src='jquery.weekcalendar.js'></script>
-  <script type='text/javascript'>
-  var year = new Date().getFullYear();
+ var year = new Date().getFullYear();
   var month = new Date().getMonth();
   var day = new Date().getDate();
 
@@ -104,18 +57,3 @@
 
     $('<div id="message" class="ui-corner-all"></div>').prependTo($('body'));
   });
-
-</script>
-</head>
-<body>
-  <h1>Week Calendar Demo</h1>
-
-  <p class="description">
-    This calendar belongs to Helena.
-      Appointments in the past are shaded grey.
-  </p>
-
-  <div id='calendar'></div>
-
-</body>
-</html>
